50f0319e17ad59ac87fa5633c91ef58463889a34
JiangHeng <tinytinywell@gmail.com>
2020-07-26 15:20:10 +0200

    initial commit

---
+++ debug_.m
@@ -0,0 +1,5 @@
+
+sim("E23");
+delay(5);
+set_param('E23/command_manual','value',num2str(1)); 
+
---
+++ helperManeuverSetUp.m
@@ -0,0 +1,8 @@
+% overtaking maneuver
+
+velocity_vehicle_B = 10; % speed of the overtaken vehicle B. Unit: m/s
+velocity_right_lane_max = 20; % maximal speed allowed on the right lane
+velocity_left_lane_max = 28; % maximal speed allowed on the left lane
+man_or_auto = 1; % 0:manual mode,1:autonomous mode
+load("Buses_.mat");
+
